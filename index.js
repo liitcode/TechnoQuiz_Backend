e7042a19347e4b3d0976714d7f4131e0a11ae85a
Divyanshu Verma <69303193+liitcode@users.noreply.github.com>
2021-06-15 22:22:28 +0530

    Updated Auth
    added more data elements due to frontend functionality

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');
+
 const app = express();
 const mongo = require('mongoose');
 const dotenv = require('dotenv');
-//Import Routes
-const authRoute = require('./routes/auth');
+
 dotenv.config();
-//Connect to DB
-mongo.connect(process.env.DB_CONNECT,{ useNewUrlParser: true,useUnifiedTopology: true },()=>console.log("Connected to DB"))
+require('./database/config');
 
-//MiddleWare
+// Import Routes
+require('./routes/cron');
+const authRoute = require('./routes/auth/auth');
+const leaderBoardRoute = require('./routes/leaderboard/leaderboard');
+const category = require('./routes/categories/categories');
+const quiz = require('./routes/quiz/quiz');
+const score = require('./routes/score/score');
+const payment = require('./routes/payment/payment');
+
+// MiddleWare
 app.use(express.json());
-
-//Route Middelware 
-app.use('/api/user',authRoute);
-
-app.listen(3000,()=> console.log("Server up and running")); 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, Auth-Token, Content-Type, Accept');
+  next();
+});
+// Route Middelware
+app.use('/api/user', authRoute);
+app.use('/api/leaderBoard', leaderBoardRoute);
+app.use('/api/category', category);
+app.use('/api/quiz', quiz);
+app.use('/api/score', score);
+app.use('/api/pay', payment);
+app.get('/', (req, res) => {
+  res.status(200).send('Server Health OK');
+});
+const port = process.env.port || 8000;
+app.listen(port);
--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,10 @@ const bcrypt = require('bcryptjs');
 
 router.post('/register',async (req,res) => {
     const { error } = registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+    if(error) return res.status(400).send({message : error.details[0].message});
 
     const emailExist = await User.findOne({email:req.body.email});
-    if(emailExist) return res.status(400).send('Email already exists');
+    if(emailExist) return res.status(400).send({ message : 'Email already exists' });
 
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password,salt);
@@ -21,24 +21,24 @@ router.post('/register',async (req,res) => {
     });
     try{
         const savedUser = await user.save();
-        res.send({user : user._id}); 
+        res.send({user : user._id,message:'Registeration Successful'}); 
     }catch(err){
-        res.status(400).send(err);
+        res.status(400).send({message : err});
     }
 });
 
 router.post('/login', async (req,res)=>{
     const { error } = loginValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+    if(error) return res.status(400).send({message : error.details[0].message});
 
     const user = await User.findOne({email:req.body.email});
-    if(!user) return res.status(400).send('Incorrect email/password'); 
+    if(!user) return res.status(400).send({message : 'Incorrect email/password'}); 
 
     const validPass = await bcrypt.compare(req.body.password,user.password);
-    if(!validPass) return res.status(400).send('Incorrect password/email');
+    if(!validPass) return res.status(400).send({message : 'Incorrect password/email'});
 
     const token = jwt.sign({id: user._id},process.env.TOKEN_SECRET);
-    const data = {username : user.name, isPremium : user.isPremium, score : user.score}
+    const data = {username : user.name, isPremium : user.isPremium, score : user.score,message: 'Login Successful',authtoken: token}
     res.header('auth-token',token).send(data);
 })
 
